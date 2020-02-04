5c57ec2bc676f5e76a358ca7a93def35b7aee393
Drozerah <drozerah@gmail.com>
2020-02-04 20:39:27 +0100

    feat: add password matching validation

--- app.js
+++ app.js
@@ -1,39 +1,100 @@
 (() => {
+  /**
+  * Check Password confirmation
+  */
+  const isPasswordsConfirmed = (data) => {
+    return new Promise((resolve, reject) => {
+      if (data.password === data.confirm) {
+        resolve(data)
+      } else {
+        const errMsg = 'Passwords do not match.'
+        reject(errMsg)
+      }
+    })
+  }
+  /**
+  * Display Error info
+  */
+  const displayError = (str) => {
+    // Get #info element
+    const display = document.querySelector('#info')
+    display.classList.add('error')
+    display.innerHTML = str
+  }
+  /**
+  * Display Success info
+  */
+  const displaySucces = (str) => {
+    // Get #info element
+    const display = document.querySelector('#info')
+    display.classList.add('success')
+    display.innerHTML = str
+    console.log(display) // !DEBUG
+  }
+  /**
+  * Reset Form 
+  */
+  const resetForm = (form) => {
+    form.reset()
+  }
+  /**
+  * Reset Visibility Icon
+  */
+  const resetVisibility = () => {
+    // reset icon
+    document.querySelector('#visibility').innerHTML = 'visibility'
+    // reset password field default type
+    const [...passwords] = document.querySelectorAll('.password')
+    passwords.forEach(element => element.type = 'password' )
+  }
+  /**
+  * Toogle Visibility Event 
+  */
+  // Get password imputs
+  const [...passwordInputs] = document.querySelectorAll('.password')
+  // Set boolean
+  let isVisibility = false
+  // Click event
+  document.querySelector('#visibility')
+    .addEventListener('click', (e) => {
+      // Toogle visibility
+      if(!isVisibility) {
+        passwordInputs.forEach(element => element.type = 'text')
+        e.target.innerHTML = 'visibility_off'
+      } else {
+        passwordInputs.forEach(element => element.type = 'password')
+        e.target.innerHTML = 'visibility'
+      }
+      isVisibility = !isVisibility
+  })
+  /**
+  * Submit Event 
+  */
+  // Get form element
+  const form = document.querySelector('#registrationForm')
+  // Submit event
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // Get form data as Object response
+    const formData = [...e.target.elements]
+      .filter(element => element.name !== 'submit' )
+      .reduce((acc, input) => ({...acc, [input.name]: input.value}), {})
+    // Validate passwords
+    isPasswordsConfirmed(formData)
+      .then(data => {
+        // Show confirmation prompt
+        confirm(JSON.stringify(data, null, 4))
+        // Display success message
+        displaySucces('Thank you!')
+        // Reset Form
+        resetForm(e.target)
+        // Reset visibility icon
+        resetVisibility()
+      })
+      .catch(err => {
+        // display err message
+        displayError(err)
+      })
+  })
 
-    /**
-    * Submit Event 
-    */
-    // Get form element
-    const form = document.querySelector('#registrationForm')
-    // Submit event
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      // Get form data as Object response
-      const formData = [...e.target.elements]
-        .filter(element => element.name !== 'submit' )
-        .reduce((acc, input) => ({...acc, [input.name]: input.value}), {})
-      console.log(formData) // !DEBUG
-      confirm(JSON.stringify(formData, null, 4))
-      e.target.reset()
-    })
-    /**
-    * Toogle Visibility Event 
-    */
-    // Get password imputs
-    const [...passwordInputs] = document.querySelectorAll('.password')
-    // Set boolean
-    let isVisibility = false
-    // Click event
-    document.querySelector('#visibility')
-      .addEventListener('click', (e) => {
-        // Toogle visibility
-        if(!isVisibility) {
-          passwordInputs.forEach(element => element.type = 'text')
-          e.target.innerHTML = 'visibility_off'
-        } else {
-          passwordInputs.forEach(element => element.type = 'password')
-          e.target.innerHTML = 'visibility'
-        }
-        isVisibility = !isVisibility
-    })
 })()
